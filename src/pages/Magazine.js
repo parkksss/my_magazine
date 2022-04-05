68b43f147e6a1f362f994aaf8741934f76c71437
parkksss <yesleee9211@gmail.com>
2022-04-05 14:27:14 +0900

    add main page style

--- src/pages/Magazine.js
+++ src/pages/Magazine.js
@@ -9,20 +9,21 @@ const Magazine = (props) => {
     <React.Fragment>
       {/* <Grid width='95%' padding='16px' margin='auto'> */}
       <Grid>
-        <Grid width='95%' padding='16px' margin='auto'>
+        <Grid width='95%' padding='16px 16px 0' margin='auto'>
           <Grid is_flex>
             <Image shape='circle' src={props.src} />
-            <div>profile / nickname / date / fix-btn</div>
+            <Text bold>{props.user_info.user_name}</Text>
+            <Text>{props.insert_dt}</Text>
           </Grid>
           <Grid>  
-            <div>content</div>
+            <Text>{props.contents}</Text>
           </Grid>
         </Grid>
         <Grid>
           <Image shape='rectangle' src={props.src} />
         </Grid>
-        <Grid width='95%' padding='16px' margin='auto'>
-          <div>comment count</div>        
+        <Grid width='95%' padding='0 16px' margin='auto'>
+          <Text bold>댓글 {props.comment_cnt}개</Text>        
         </Grid>
       </Grid>
     </React.Fragment>
