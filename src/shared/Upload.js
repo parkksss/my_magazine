68a33b3bbc0b9f704ea5a56bc2ac323ad1545b56
parkksss <yesleee9211@gmail.com>
2022-04-06 16:36:43 +0900

    add function - image upload with firebase storage

--- src/shared/Upload.js
+++ src/shared/Upload.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Button } from "../elements";
+import { storage } from './firebase';
 
 const Upload = (props) => {
+    const fileInput = React.useRef();
+
+    const selectFile = (e) => {
+        console.log(e)
+        console.log(e.target); 
+        console.log(e.target.files[0]); 
+
+
+        console.log(fileInput.current.files[0]); 
+    }
+
+    const uploadFB = () => {
+        let image = fileInput.current?.files[0];
+        const _upload = storage.ref(`images/${image.name}`).put(image);
+  
+        _upload.then((snapshot) => {
+          console.log(snapshot);
+
+          snapshot.ref.getDownloadURL().then((url) => {
+              console.log(url);
+          });
+        });
+  
+    }
 
     return (
         <React.Fragment>
-            <input type="file"/>
+            <input onChange={selectFile} type="file" ref={fileInput}/>
+            <Button _onClick={uploadFB}>업로드하기</Button>
         </React.Fragment>
     )
 }
